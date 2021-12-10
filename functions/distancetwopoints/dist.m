a5c1d2753dac9cfcf7e08ef7540909f57764d494
Havva Yılmaz <86909580+thehavva@users.noreply.github.com>
2021-12-10 23:47:08 +0300

    Add files via upload

---
+++ functions/distancetwopoints/dist.m
@@ -0,0 +1,7 @@
+function distance=dist(x1,y1,x2,y2)
+% DIST Calculate the distance between two points
+% Function DIST2 calculates the distance between
+% two points (x1,y1) and (x2,y2) in a Cartesian
+% coordinate system.
+distance=sqrt((x1-x2)^2+(y1-y2)^2);
+end
---
+++ functions/distancetwopoints/distancewithfunction.m
@@ -0,0 +1,8 @@
+% calculate the distance between two certain point as input of x and y axis.
+x1=input('first x: ');
+y1=input('first y: ');
+x2=input('second x: ');
+y2=input('second y: ');
+
+f=dist(x1,y1,x2,y2);
+disp(num2str(f));
